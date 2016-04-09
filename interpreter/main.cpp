3b8b14c35d3f33d041e37f8583c2ec253309989b
sergio vieira <sergiosvieira@gmail.com>
2016-04-09 07:07:45 -0300

    Initial Commit

---
+++ interpreter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  interpreter
+//
+//  Created by sergio vieira on 09/04/16.
+//  Copyright © 2016 sergio vieira. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
